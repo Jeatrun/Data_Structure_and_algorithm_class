24b379ee88bbbc5dce818e2b657fa66a129285e8
KodomoHan <2821117052@qq.com>
2023-09-14 20:29:56 +0800

    code for week2/exp1

--- lab/week2/exp1/Stu.h
+++ lab/week2/exp1/Stu.h
@@ -1,11 +1,13 @@
+//定义结构体类型date
 typedef struct{
     int day,month,year;
 
 } date ;
 
+//定义结构体类型stu
 typedef struct{
     char name[20];
     long num;
-    date birthday;
+    date birthdate;
 }stu;
 
--- lab/week2/exp1/main.cpp
+++ lab/week2/exp1/main.cpp
@@ -3,14 +3,39 @@
 #include <malloc.h>
 
 int main(){
-    stu *student1;
-    student1=(stu*)malloc(sizeof(stu));
-    student1->num=30;
-    printf("%ld\n",student1->num);
+    stu *student1;//定义结构体指针变量
+    student1=(stu*)malloc(sizeof(stu));  
+    //input
+    printf("\n\tstudent 1\n");
+    printf("please enter the name: ");
+    scanf("%s",student1->name);
+    printf("please enter the number: ");
+    scanf("%ld",&student1->num);
+    printf("please enter the birthdate(yyyy.mm.dd): \n");
+    scanf("%d.%d.%d",&student1->birthdate.year,&student1->birthdate.month,&student1->birthdate.day);
+    
+    //output
+    printf("\n\tInformation of student1\n");
+    printf("name: %s\tnumber: %ld\n",student1->name,student1->num);
+    printf("birthdate:%d.%d.%d\n",student1->birthdate.year,student1->birthdate.month,student1->birthdate.day);
+    
+    free(student1);
 
     stu student2;//定义结构体变量
-    student2.num=13;
-    printf("%ld\n",student2.num);
+    //input
+    printf("\n\t student 2\n");
+    printf("please enter the name: ");
+    scanf("%s",student2.name);
+    printf("please enter the number: ");
+    scanf("%ld",&student2.num);
+    printf("please enter the birthdate(yyyy.mm.dd): \n");
+    scanf("%d.%d.%d",&student2.birthdate.year,&student2.birthdate.month,&student2.birthdate.day);
+    
+    //output
+    printf("\n\tInformation of student2:\n");
+    printf("name: %s\tnumber: %ld\n",student2.name,student2.num);
+    printf("birthdate:%d.%d.%d\n",student2.birthdate.year,student2.birthdate.month,student2.birthdate.day);
+
 
 
     return 0;
