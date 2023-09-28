98f0d4d439f7798a51cedde157a0d423b703e5d7
KodomoHan <2821117052@qq.com>
2023-09-28 11:02:04 +0800

    fixed bugs for lab/week4

--- lab/week4/LinkList.h
+++ lab/week4/LinkList.h
@@ -1,37 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define MAXSIZE 100
-//definition
-typedef int ElemType ;
-typedef struct LNode{
+// definition
+typedef int ElemType;
+typedef struct LNode
+{
 
     ElemType data;
 
     struct LNode *next;
 } LinkNode;
 
-//create
-void CreateListR(LinkNode *&L,ElemType a[],int n){
-    LinkNode *s,*r;
-    r=L;
-    //1.create a empty list
+// create
+void CreateListR(LinkNode *&L, ElemType a[], int n)
+{
+    /*
+    错误代码:
+    LinkNode *s, *r;
+    r = L;
     L = (LinkNode *)malloc(sizeof(LinkNode));
-    //2.take every elem in a[i], create a new node ,insert at the end of the list
-    for(int i=0;i<n;i++){
+
+    错误总结:在L分配新空间前先把L赋值给r,导致给L分配空间后r所指向的内存空间已不是链表,以至于最后的数值无法传给链表
+    */
+
+    LinkNode *s, *r; // s : new node r:final(current)node
+    // 1.create a empty list
+    L = (LinkNode *)malloc(sizeof(LinkNode));
+    r = L;          // let the pointer point at head node
+    L->next = NULL; // initialize the next pointer of head node to null
+
+    // 2.create a new node ,take every elem at a[i] into the data , insert the node at the end of the list
+    for (int i = 0; i < n; i++)
+    {
         s = (LinkNode *)malloc(sizeof(LinkNode));
         s->data = a[i];
         s->next = NULL;
         r->next = s;
+        r = r->next;
     }
-    //3.set the next pointer of the last node to null
-
-    r->next = NULL;
 }
 
-void DispList(LinkNode *L){
+void DispList(LinkNode *L)
+{
     LinkNode *p = L->next;
-    while(p!=NULL){
-        printf("%4d ",p->data);
+    while (p != NULL)
+    {
+        printf("%4d ", p->data);
         p = p->next;
-        }
+    }
 }
--- lab/week4/main.cpp
+++ lab/week4/main.cpp
@@ -1,6 +1,9 @@
 #include "LinkList.h"
-int main(){
-    int A[5]={1,2,3,4,5};
+int main()
+{
+    int A[5] = {1, 2, 3, 4, 5};
     LinkNode *L1;
-     CreateListR(L1,A,5);
+    CreateListR(L1, A, 5);
+    DispList(L1);
+    return 0;
 }
