71a8c0a3aba02b6dc9b3f770e7da7a45dce9b9b4
KodomoHan <2821117052@qq.com>
2023-10-24 16:07:32 +0800

    improved LinkQueue

--- lab/week7/LinkQueue.h
+++ lab/week7/LinkQueue.h
@@ -1,95 +1,138 @@
 #include <malloc.h>
-#include <stdio.h> 
+#include <stdio.h>
 
-typedef int ElemType;
+typedef char* ElemType;
+typedef struct qnode
 
-//----- 队列的链式存储-----
-typedef struct qnode 
-{ 			// 结点类型
-     ElemType  data;
-     struct qnode  *next;
-} QNode;
-typedef struct 
-{ // 链队列类型
-      QNode*  front;  // 队头指针
-      QNode*  rear;    // 队尾指针
-}LiQueue;
-
-
-void InitQueue(LiQueue *&q)
-{  
-	q=(LiQueue *)malloc(sizeof(LiQueue));
-    q->front=q->rear=NULL;
-}
-void DestroyQueue(LiQueue *&q)
-{  
-	QNode *p=q->front,*r;  //p指向队头数据节点
-    if (p!=NULL)		  //释放数据节点占用空间
-    {  
-		r=p->next;
-		while (r!=NULL)
-		{   
-			free(p);
-	   		p=r;
-			r=p->next;
-		}
-   }
-   free(p);
-   free(q);	  //释放链队节点占用空间
-}
-bool QueueEmpty(LiQueue *q)
 {
-	return(q->rear==NULL);
+    ElemType data;
+    qnode *next;
+
+} LinkQNode;
+
+typedef struct
+{
+    LinkQNode *front;
+    LinkQNode *rear;
+
+} LinkQueue;
+
+void InitLinkQueue(LinkQueue *&q)
+{
+    q = (LinkQueue *)malloc(sizeof(LinkQueue));
+    q->front = q->rear = NULL;
 }
-void enQueue(LiQueue *&q,ElemType e)
-{  
-	QNode *p;
-    p=(QNode *)malloc(sizeof(QNode));
-    p->data=e;
-    p->next=NULL;
-    if (q->rear==NULL) //若链队为空,新节点是队首节点又是队尾节点
-		q->front=q->rear=p;
+
+void DestroyLinkQueue(LinkQueue *&q)
+{
+    LinkQNode *p1, *p2;
+    if (q == NULL)
+    {
+        printf("the queue doesn't exist!");
+        return;
+    }
+    p1 = q->front;
+    while (p1 != NULL)
+    {
+        p2 = p1->next;
+        free(p1);
+        p1 = p2;
+    }
+    free(p1);
+    free(q);
+}
+
+bool QueueEmpty(LinkQueue *q)
+{
+    return (q->front == NULL);
+}
+
+void EnQueue(LinkQueue *&q, ElemType e)
+{
+    LinkQNode *p;
+    p = (LinkQNode *)malloc(sizeof(LinkQNode));
+
+    /*
+    exited problem:
+        p->data = e;
+        p->next = NULL;
+    reason:
+        deliver a same pointer 'e' so that every data was pointed at a same space when changing 'e'
+    */
+
+    p->data = (ElemType)malloc(100);
+    
+    //copy e to data
+    int i = 0;
+    while (e[i] != '\0')
+    {
+        p->data[i] = e[i];
+        i++;
+    }
+    p->data[i] = '\0';
+
+    p->next = NULL;
+    if (q->front == NULL)
+    {
+        q->front = q->rear = p;
+    }
     else
-    {  
-		q->rear->next=p;//将*p节点链到队尾,并将rear指向它
-		q->rear=p;
+    {
+        q->rear->next = p;
+        q->rear = p;
     }
 }
-bool deQueue(LiQueue *&q,ElemType &e)
-{  
-	QNode *t;
-    if (q->rear==NULL) 
-		return false;	//队列为空
-    t=q->front;		   //t指向第一个数据节点
-    if (q->front==q->rear)  //队列中只有一个节点时
-		q->front=q->rear=NULL;
-    else			   //队列中有多个节点时
-		q->front=q->front->next;
-    e=t->data;
-    free(t);
-    return true;
-}
-
-void DispQueue(LiQueue *q)   
-{	//从队头到队尾，逐个显示出来 
-	QNode *p= q->front;
-	while(p != NULL)
-	{
-		printf("%4d",p->data);
-		p = p->next;
-	}
-}
-
-int LengthQueue(LiQueue *q)
+bool DeQueue(LinkQueue *&q, ElemType &e)
 {
-	if (q->rear==NULL) 
-		return 0;	//队列为空
-	int cout=0;
-	QNode *p= q->front;
-	while(p != NULL)
-	{
-		cout++;
-		p = p->next;
-	}
-	return cout;
+    LinkQNode *p;
+    if (!QueueEmpty(q))
+    {
+        p = q->front;
+        e = p->data;
+        q->front = p->next;
+        free(p);
+        return true;
+    }
+    else
+        return false;
+}
+
+void DisplayQueue(LinkQueue *q)
+{
+
+    if (!QueueEmpty(q))
+    {
+        LinkQNode *p;
+        p = q->front;
+        while (p != NULL)
+        {
+            printf("%4s", p->data);
+            p = p->next;
+        }
+    }
+    else
+    {
+        printf("Queue is empty!");
+    }
+}
+
+int LengthQueue(LinkQueue *q)
+{
+    if (!QueueEmpty(q))
+    {
+        LinkQNode *p;
+        p = q->front;
+        int count = 0;
+        while (p != NULL)
+        {
+            p = p->next;
+            count++;
+        }
+        return count;
+    }
+    else
+    {
+        printf("Queue is empty!");
+        return 0;
+    }
 }
--- lab/week7/main.cpp
+++ lab/week7/main.cpp
@@ -1,44 +1,58 @@
 #include "LinkQueue.h"
 #include <stdio.h>
 #include <stdlib.h>
-int main(){
-    LiQueue *q;
-    InitQueue(q);
-    //menu
+int main()
+{
+    LinkQueue *q;
+    InitLinkQueue(q);
+    // menu
     int choice;
-    while(1){
-        ElemType num;
-        printf("1.排队\n2.就诊\n3.查看队列\n4.下班\n");
+    while (1)
+    {
+        ElemType str_num;
+        printf("1.Enqueue\n2.Visit\n3.Display the queue\n4.Off work\n");
         printf("Please choose an option:");
-        scanf("%d",&choice);
-        switch(choice){
-            case 1:
-                int queNum;
-                printf("please enter the patients' number that want to queue: ");
-                scanf("%d",&queNum);
-                for(int i=queNum;i>0;i--){
-                    printf("please enter your number: ");
-                    scanf("%d",&num);
-                    enQueue(q,num);
-                }
-                //system("pause");
-                //system("cls");
-                break;
-            case 2:
-                deQueue(q,num);
-                //system("pause");
-                //system("cls");
-                break;
-            case 3:
-                 DispQueue(q);
-                // system("pause");
-                //system("cls");
-                break;
-            case 4:
-                DestroyQueue(q);
-                // system("pause");
-                //system("cls");
-                break;
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            int queNum;
+            char str[100];
+            printf("please enter the patients' number that want to enqueue: ");
+            scanf("%d", &queNum);
+            for (int i = 0; i < queNum; i++)
+            {
+                printf("please enter the number(%d/%d): ", i + 1, queNum);
+                scanf("%s", str);
+                str_num = str;
+                EnQueue(q, str_num);
             }
-    }    
+            system("pause");
+            system("cls");
+            break;
+        case 2:
+            if (DeQueue(q, str_num))
+            {
+                printf("patient %s has visited the doctor\n", str_num);
+            }
+            else
+            {
+                printf("queue is empty\n");
+            };
+            system("pause");
+            system("cls");
+            break;
+        case 3:
+            DisplayQueue(q);
+            printf("\n");
+            system("pause");
+            system("cls");
+            break;
+        case 4:
+            DestroyLinkQueue(q);
+            system("pause");
+            system("cls");
+            return 0;
+        }
+    }
 }
