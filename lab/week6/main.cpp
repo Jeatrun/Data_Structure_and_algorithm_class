ec64c40392647175d9ef1f3ecd2f3906d71b2489
KodomoHan <2821117052@qq.com>
2023-10-25 23:07:42 +0800

    fix

--- lab/week6/main.cpp
+++ lab/week6/main.cpp
@@ -1,9 +1,11 @@
 #include <malloc.h>
-//#include "exp1.h"
-#include "Is_symmetry.h"
-//#include "Symbol_If_The_Same.h"
+// #include "exp1.h"//第一题
+#include "Is_symmetry.h" //第三题
+// #include "Symbol_If_The_Same.h"//第二题
 int main()
 {
-    exp3();
+    // exp1();//第一题
+    // exp2();//第二题
+    exp3(); // 第三题
     return 0;
 }
