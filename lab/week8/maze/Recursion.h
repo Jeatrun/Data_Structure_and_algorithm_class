51db13e3ea404222f6edbd1a7cb77ba43834185c
KodomoHan <2821117052@qq.com>
2023-11-08 09:46:51 +0800

    mazeproblem-ver0

---
+++ lab/week8/maze/Recursion.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+
+} maze;
+
+void FindMaze(int xi, int yi, int xe, int ye, maze maze)
+{
+    int curX = xi, curY = yi;
+    // base case
+    if (curX == xe && curY == ye)
+    {
+        // what should the base case return?
+    }
+}
