77c9a41478339f3a48cf92e0e44ee840d9b6f3ab
2821117052 <2821117052@qq.com>
2023-11-19 08:01:17 +0000

    adMatrix

---
+++ self/graphMatrix/AdjacencyMatrix/Am.h
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define  MAX 100
+#define INFINITY 1000000000
+typedef struct{
+    char vexs[MAX];
+    int adMatrix[MAX][MAX];
+    int vNum,eNum;
+} mgraph;
+
+void initMatrix(mgraph *&G){
+    for(int i=0;i<G->vNum;i++){
+        for(int j=0;j<G->vNum;j++){
+            G->adMatrix[i][j]=INFINITY;
+        }
+    }
+}
+
+void createMGraph(mgraph *&G){
+    G=(mgraph*)malloc(sizeof(mgraph));
+    //enter the nums of vexs and edges
+    printf("plase enter the nums of vexs:");
+    scanf("%d",&G->vNum);
+    printf("plase enter the nums of edges:");
+    scanf("%d",&G->eNum);
+  
+    //enter the vexs
+    for(int i=0;i<G->vNum;i++){
+        printf("please enter the %d th vex's value: ",i+1);
+        scanf(" %c",&G->vexs[i]);
+    }
+    //initiate the matrix
+    initMatrix(G);
+
+    //enter the weight of the edges
+    for(int i=0;i<G->eNum;i++){
+        int v1,v2,w;
+        printf("please enter the %d th edge's v1,v2,w: ",i+1);
+        scanf("%d,%d,%d",&v1,&v2,&w);
+        G->adMatrix[v1][v2]=w;
+    }
+
+}
+void printMGraph(mgraph *G){
+    for(int i=0;i<G->vNum;i++){
+        for(int j=0;j<G->vNum;j++){
+            if(G->adMatrix[i][j]==INFINITY){
+                printf("∞ ");
+            }
+            else {
+                printf("%d ",G->adMatrix[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
---
+++ self/graphMatrix/AdjacencyMatrix/main.cpp
@@ -0,0 +1,8 @@
+#include"Am.h"
+
+int main(){
+    mgraph *G;
+    createMGraph(G);
+    printMGraph(G);
+
+}
